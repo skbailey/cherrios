1d261c95dd1e36a8ebff5c995d5f78d81cd724ba
Sherard Bailey <sherard@uniteus.com>
2020-10-31 01:56:20 -0400

    Add ProfileVC

--- cherrios/ProfileTableViewController.swift
+++ cherrios/ProfileTableViewController.swift
@@ -43,11 +43,11 @@ class ProfileTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return stats.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "profileCellIdentifier", for: indexPath)
         
         let stat = stats[indexPath.row]
         cell.textLabel?.text = stat.name
